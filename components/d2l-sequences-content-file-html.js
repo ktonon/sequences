c18ebe7ab9fb2bfac3a11a44741cb57676d624d5
Chris Kraljevic <Chris.Kraljevic@D2L.com>
2019-12-10 13:50:48 -0600

    oops

--- components/d2l-sequences-content-file-html.js
+++ components/d2l-sequences-content-file-html.js
@@ -20,7 +20,7 @@ export class D2LSequencesContentFileHtml extends D2L.Polymer.Mixins.Sequences.Au
 		<template is="dom-if" if="[[_usePdfViewer]]">
 			<d2l-pdf-viewer
 				src="[[_fileLocation]]"
-				pdf-js-worker-src="https://pdfjs-dist-modules-test.s3.us-east-2.amazonaws.com/pdf.worker.min.js"
+				pdf-js-remote-path="https://pdfjs-dist-modules-test.s3.us-east-2.amazonaws.com"
 			>
 			</d2l-pdf-viewer>
 		</template>
