43d028fdc4b083de417371490a2df51798246fd7
Chris Kraljevic <chris.kraljevic@d2l.com>
2019-12-19 15:29:04 -0600

    DE36376 Using d2l-pdf-viewer (#170)
    * DE36376 Using d2l-pdf-viewer
    
    * Use pdf component for converted file test

---
+++ components/d2l-sequences-content-file-pdf.js
@@ -0,0 +1,68 @@
+import '../mixins/d2l-sequences-automatic-completion-tracking-mixin.js';
+import 'd2l-pdf-viewer/d2l-pdf-viewer.js';
+import { html } from '@polymer/polymer/lib/utils/html-tag.js';
+export class D2LSequencesContentFilePdf extends D2L.Polymer.Mixins.Sequences.AutomaticCompletionTrackingMixin() {
+	static get template() {
+		return html`
+		<style>
+			d2l-pdf-viewer {
+				width: 100%;
+				height: calc(100% - 12px);
+				overflow: hidden;
+			}
+		</style>
+		<d2l-pdf-viewer
+			src="[[_fileLocation]]"
+			loader="script"
+			use-cdn
+		>
+		</d2l-pdf-viewer>
+`;
+	}
+
+	static get is() {
+		return 'd2l-sequences-content-file-pdf';
+	}
+	static get properties() {
+		return {
+			href: {
+				type: String,
+				reflectToAttribute: true,
+				notify: true,
+				observer: '_scrollToTop'
+			},
+			_fileLocation: {
+				type: String,
+				computed: '_getFileLocation(entity)'
+			},
+			title: {
+				type: String,
+				computed: '_getTitle(entity)'
+			},
+		};
+	}
+
+	disconnectedCallback() {
+		super.disconnectedCallback();
+		window.postMessage(JSON.stringify({ handler: 'd2l.nav.reset' }), '*');
+	}
+
+	_scrollToTop() {
+		window.top.scrollTo(0, 0);
+	}
+
+	_getFileLocation(entity) {
+		try {
+			const fileActivity = entity.getSubEntityByClass('file-activity');
+			const file = fileActivity.getSubEntityByClass('file');
+			const link = file.getLinkByClass('pdf') || file.getLinkByClass('embed') || file.getLinkByRel('alternate');
+			return link.href;
+		} catch (e) {
+			return '';
+		}
+	}
+	_getTitle(entity) {
+		return entity && entity.properties && entity.properties.title || '';
+	}
+}
+customElements.define(D2LSequencesContentFilePdf.is, D2LSequencesContentFilePdf);
