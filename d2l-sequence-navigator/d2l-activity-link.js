b1cb85d69da902a5d44b77e2133e2da45a7f5b64
Ian McDonald <64804046+imcdonald-d2l@users.noreply.github.com>
2020-07-02 13:30:11 -0400

    Activity click issue (#243)
    * Adjust on-click functionality
    
    * switch back to <a> tag

--- d2l-sequence-navigator/d2l-activity-link.js
+++ d2l-sequence-navigator/d2l-activity-link.js
@@ -170,13 +170,13 @@ class D2LActivityLink extends PolymerASVLaunchMixin(CompletionStatusMixin()) {
 				<div id="skeleton" class="skeleton"></div>
 			</template>
 			<template is="dom-if" if="[[!showLoadingSkeleton]]">
-				<div id="content-container" on-click="_contentObjectClick" tabindex="0">
+				<div id="content-container" on-click="setCurrent" tabindex="0">
 					<div id="title-container">
 						<template is="dom-if" if="[[hasIcon]]">
 							<d2l-icon icon="[[_getIconSetKey(entity)]]"></d2l-icon>
 						</template>
 						<div class="d2l-activity-link-title">
-							<a on-click="setCurrent" class$="[[completionRequirementClass]]" href="javascript:void(0)" tabindex="-1">
+							<a class$="[[completionRequirementClass]]" href="javascript:void(0)" tabindex="-1">
 								[[entity.properties.title]]
 							</a>
 							<d2l-completion-requirement href="[[href]]" token="[[token]]">
