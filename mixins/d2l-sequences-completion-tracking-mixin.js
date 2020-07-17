77c27dd10561ef77dc30ad95d71c6aa8d1fe25ee
Ian McDonald <64804046+imcdonald-d2l@users.noreply.github.com>
2020-07-17 09:10:57 -0400

    Clean up completion mixin and add delay (#250)
    * clean up mixin
    
    * Remove broken unit test
    
    * fix broken completion status components
    
    * undo link component changes
    
    * remove unnecessary then statement
    
    * set completion to null once complete

--- mixins/d2l-sequences-completion-tracking-mixin.js
+++ mixins/d2l-sequences-completion-tracking-mixin.js
@@ -5,6 +5,9 @@ import '../localize-behavior.js';
 import { Maybe } from '../maybe.js';
 import { mixinBehaviors } from '@polymer/polymer/lib/legacy/class.js';
 import { PolymerElement } from '@polymer/polymer/polymer-element.js';
+
+const FINISH_COMPLETION_DELAY = 6000;
+
 function CompletionTrackingMixin() {
 	return class extends mixinBehaviors([
 		D2L.PolymerBehaviors.Siren.EntityBehavior,
@@ -18,9 +21,6 @@ function CompletionTrackingMixin() {
 				_completionEntity: {
 					type: Object
 				},
-				_failedCompletion: {
-					type: Object
-				},
 				_skipCompletion: {
 					type: Boolean,
 					computed: '_isImpersonating(token)'
@@ -29,15 +29,14 @@ function CompletionTrackingMixin() {
 		}
 
 		finishCompletion() {
-			this._fireToastEvent();
-
 			if (!this._completionEntity || this._skipCompletion) {
 				return;
 			}
 
-			this._performViewActions(this._completionEntity, 'finish-view-activity')
-				.then(() => { this._completionEntity = null; })
-				.catch(() => { this._completionEntity = null; });
+			setTimeout(() => {
+				this._performViewActions(this._completionEntity, 'finish-view-activity')
+					.then(() => { this._completionEntity = null; });
+			}, FINISH_COMPLETION_DELAY);
 		}
 
 		startCompletion() {
@@ -46,8 +45,7 @@ function CompletionTrackingMixin() {
 			}
 
 			this._performViewActions(this.entity, 'view-activity-duration')
-				.then(completion => this._completionEntity = completion)
-				.catch(entity => this._failedCompletion = entity);
+				.then(completion => this._completionEntity = completion);
 		}
 
 		_performViewActions(entity, actionName) {
@@ -81,76 +79,6 @@ function CompletionTrackingMixin() {
 			} catch (e) {
 				return false;
 			}
-		}
-
-		_fireToastEvent() {
-			const activity = Maybe.of(this._failedCompletion)
-				.map(e => e.getSubEntityByClass('activity'));
-
-			if (activity.isNothing()) {
-				this._failedCompletion = null;
-				return;
-			}
-
-			const complete = activity.map(
-				a => a.getSubEntityByClass('completed')
-			);
-
-			if (!complete.isNothing()) {
-				this._failedCompletion = null;
-				return;
-			}
-
-			// Due to the router / entity store, we may run this
-			// twice during initial load and we don't want to fire
-			// the toast event if we're just refreshing our own
-			// data.
-			const currentActivity = Maybe.of(this.entity)
-				.chain(
-					e => e.getSubEntityByClass('activity'),
-					a => a.getLinkByRel('about'),
-					p => p.href
-				);
-
-			const oldActivity = activity.chain(
-				a => a.getLinkByRel('about'),
-				p => p.href
-			);
-
-			if (currentActivity.value === oldActivity.value) {
-				this._failedCompletion = null;
-				return;
-			}
-
-			const href = activity.chain(
-				a => a.getLinkByRel('about'),
-				a => a.href
-			).value;
-
-			if (!href) {
-				this._failedCompletion = null;
-				return;
-			}
-
-			const notifyActivityTypes = [
-				'dropbox',
-				'quiz',
-				'discuss'
-			];
-
-			if (notifyActivityTypes.some(t => href.includes(t))) {
-				const event = new CustomEvent('toast', {
-					detail: {
-						message: 'There was more to do in the last activity.',
-						name: this._failedCompletion.properties.title,
-						url: this._failedCompletion.getLinkByRel('self').href
-					},
-					bubbles: true,
-					composed: true,
-				});
-				window.dispatchEvent(event);
-			}
-			this._failedCompletion = null;
 		}
 	};
 }
